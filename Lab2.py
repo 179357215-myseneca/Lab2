e2ef70b48d509df9c77ab45290ef4d9ef312dea6
179357215-myseneca <adookie4@myseneca.com>
2023-06-08 19:54:11 -0400

    lab2 function

---
+++ Lab2.py
@@ -0,0 +1,10 @@
+import sys
+
+def print_arguments():
+    script_name = sys.argv[0]
+    arguments = ' '.join(sys.argv[1:])
+    print(f"Script: {script_name}")
+    print(f"Script & Variables: {script_name} {arguments}")
+
+print_arguments()
+
